d998ea09f810737c648472b62111f47ce3c9b52c
Sergey <sergey400@mail.ru>
2021-09-26 22:44:08 +0300

    changes

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,78 +1,93 @@
 ﻿
+
 #include <iostream>
+#include "MyLib.h"
+#include <fstream>
+
+
+
+
+#define TRUEORFALSE(vvedenoe_chislo, konechnoe_chislo) ((vvedenoe_chislo >= 0 && vvedenoe_chislo < konechnoe_chislo)) 
+#define ZADAEM_CHISLO(n, e_byte)
 
 using namespace std;
 
+
+
 void obzac()
 {
-    cout << "//====================================HW====================================//\n\n";
+    cout << "\n\n//====================================HomeWork====================================//\n\n";
 }
-double ps5[696];
-void dz2(int* x, const int v)
-{
-    for (int z = 0; z < v; z++)
-        if (x[z]) x[z] = 0;
-        else x[z] = 1;
-
-
-}
-
-void Print(int* x, const int v)
-{
-    for (int z = 0; z < v; z++)
-        cout << x[z] << " ";
-    cout << "\n";
-}
-
-
-
-bool PrintPizdc(double ps5[], int size)
-{
-    if (ps5 == nullptr || size == 0)
-        return false;
-
-    for (size_t a = 0; a <size; a++)
-
-        cout << a << " : " << ps5[a] << " ";
-
-    return true;
-    cout << endl;
-}
-
-void xrenaten(int* a, const int n)
-{
-    for (int i = 0; i < n; i++)
-        if (a[i]) a[i] = 0; else a[i] = 1;
-}
-
-
 int main()
 {
     {
+        
         obzac();
-        PrintPizdc(ps5, 696);
 
-        const int n = 10;
-        int a[n] = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
 
-        Print(a, n);
-        xrenaten(a, n);
-        Print(a, n);
+        const int size = 4;
+        float* ptr = 0;
+        ptr = MyLib::f1(size);
+        MyLib::f2(ptr, size);
 
-        system("pause");
+        delete[]ptr;
+
     }
+
+    {
+        
+        obzac();
+
+        int n_nnoe_chislo;
+        const int MULTIOTREZOK = 666;
+        cout << boolalpha; 
+        cout << "Введите число, которое хочешь проверить: " << endl;
+        cin >> n_nnoe_chislo;
+        bool proveryaemoe_chislo = TRUEORFALSE(n_nnoe_chislo, MULTIOTREZOK);
+        cout << "Результат: " << proveryaemoe_chislo << endl;
+
+
+    }
+
     {
         obzac();
 
-        int array[8];
-        array[0] = 1;
-        for (int i = 1; i < 8; i++) {
-            array[i] = array[i - 1] + 3;
+        ZADAEM_CHISLO(n, e_byte)
+            int* e_byte;
+        int n;
+        cout << "вставьте число: ";
+        cin >> n;
+        e_byte = new int[n];
 
+        for (int i = 0; i < n; i++)
+        {
+            cout << "e_byte[" << i << "] = ";
+            cin >> e_byte[i];
         }
-        Print(array, 8);
-    }
+        int temp; 
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (e_byte[j] > e_byte[j + 1])
+                {
+                   
+                    temp = e_byte[j];
+                    e_byte[j] = e_byte[j + 1];
+                    e_byte[j + 1] = temp;
+                }
+            }
+        }
+
+        
+        for (int i = 0; i < n; i++) {
+            cout << e_byte[i] << " ";
+        }
+        cout << endl;
+
+        delete[] e_byte; 
+
+    }    
     return 0;
-
 }
-
